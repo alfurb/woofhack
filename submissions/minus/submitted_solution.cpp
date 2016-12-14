fde44568634823cc4a2d261d1e0206b692442e9c
Sigurjón Freyr <siggifreyr1@gmail.com>
2016-12-14 15:36:32 +0000

    Will classify as Denied or Accepted

--- submissions/minus/submitted_solution.cpp
+++ submissions/minus/submitted_solution.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
   int a, b;
   cin >> a >> b;
-  cout << a + b;
+  cout << a - b;
   char bla[] = "asdf";
   return 0;
 }
--- web_server.py
+++ web_server.py
@@ -2,6 +2,8 @@ from flask import Flask, request, session, g, redirect, url_for, abort, render_t
 from mako.template import Template
 import os
 import sqlite3
+import subprocess
+from enum import Enum
 
 
 # create our little application :)
@@ -17,30 +19,61 @@ app.config.update(dict(
 ))
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
+# Specify classes
+class Classification():
+    Accepted = "Accepted"
+    Denied = "Denied"
+    Error = "Error"
+
+class Problem():
+    name = ""
+    description = ""
+    tests = []
+    def __init__(self):
+        self.tests = list([])
+    def __str__(self):
+        return str(self.__class__) + ": " + str(self.__dict__)
+    def __repr__(self):
+        return str(self)
+
+class Test():
+    name = ""
+    inp = ""
+    out = ""
+    def __str__(self):
+        return str(self.__class__) + ": " + str(self.__dict__)
+    def __repr__(self):
+        return str(self)
+
 # Load the problems
 problems = {}
 for name in os.listdir('problems'):
     base_path = os.path.join('problems', name)
-    problem = {}
-    problem["name"] = name
-    problem["description"] = ""
+    problem = Problem()
+    problem.name = name
     with open(os.path.join(base_path, 'description.html')) as f:
-        problem["description"] = f.read()
-    problem["tests"] = []
+        problem.description = f.read()
     has_solution = os.path.exists(os.path.join(base_path, 'solution.py'))
     test_base_dir = os.path.join(base_path, 'tests')
     assert os.path.exists(test_base_dir)
     for tname in os.listdir(test_base_dir):
         test_dir = os.path.join(test_base_dir, tname)
-        test = {}
-        test["name"] = tname
-        test["input"] = os.path.join(test_dir, 'in')
-        test["output"] = os.path.join(test_dir, 'out')
-        assert os.path.exists(test["input"])
-        assert has_solution or os.path.exists(test["output"])
-        problem["tests"].append(test)
+        test = Test()
+        test.name = tname
+        # Read input
+        test.inp = os.path.join(test_dir, 'in')
+        with open(test.inp) as f:
+            # Strip trailing newline
+            test.inp = f.read().strip()
+        # Read in expected output
+        test.out = os.path.join(test_dir, 'out')
+        with open(test.out) as f:
+            # Strip trailing newline
+            test.out = f.read().strip()
+        problem.tests.append(test)
     problems[name] = problem
 print (problems)
+
 
 @app.route("/")
 @app.route("/index")
@@ -49,14 +82,43 @@ def index():
 
 @app.route("/submit/<problem>", methods=['GET', 'POST'])
 def submit(problem):
+    if problem not in problems:
+        abort(404)
     if request.method == 'GET':
         return Template(filename="templates/submit.html").render(name=problem)
     f = request.files['file']
-    print(f)
+
     path = os.path.join('submissions', problem)
     os.makedirs(path, exist_ok=True)
-    f.save('submissions/{0}/submitted_solution.cpp'.format(problem))
-    return redirect("/")
+    file_path = os.path.join(path, 'submitted_solution.cpp')
+    f.save(file_path)
+
+    res = run(problem, path, file_path, "c++")
+    return Template(filename="templates/result.html").render(
+        classification=res[0],
+        message=str(res[1], encoding="utf8").replace("\n", "<br>"),
+        accepted=res[0] == Classification.Accepted)
+
+def run(problem, submission_folder_path, file_path, language):
+    """returns a tuple (classification, message)"""
+    if language == "c++":
+        output_path = os.path.join(submission_folder_path, 'compiled')
+        p = subprocess.Popen(["g++", "-o", output_path, file_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+        output = p.communicate()
+        print("compile", output)
+        if output[1]:
+            return (Classification.Error, output[1])
+        accepted = True
+        print(problems[problem])
+        for i in problems[problem].tests:
+            print (i)
+        for test in problems[problem].tests:
+            p = subprocess.Popen(["./" + output_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+            output = p.communicate(input=test.inp.encode())
+            print ("run", output)
+            print("res", output[0].decode(), "expected", test.out, "input", test.inp)
+            accepted = accepted and output[0].decode() == test.out
+        return (Classification.Accepted if accepted else Classification.Denied, output[0])
 
 def connect_db():
     """Connects to the specific database."""
